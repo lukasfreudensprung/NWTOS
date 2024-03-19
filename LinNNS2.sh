f25eb8085c1c27ee52aef3244f9bbc9a11bdd4e7
lukasfreudensprung <127208026+lukasfreudensprung@users.noreply.github.com>
2024-03-19 11:52:31 +0100

    Update LinNNS2.sh

--- LinNNS2.sh
+++ LinNNS2.sh
@@ -28,6 +28,6 @@ sudo sysctl -w net.ipv4.ip_forward=1
 sudo sysctl -w net.ipv6.conf.all.forwarding=1
 sudo sysctl -p
 
-sudo curl -s https://raw.githubusercontent.com/lukasfreudensprung/NWTOS/main/ipt.sh
+sudo curl https://raw.githubusercontent.com/lukasfreudensprung/NWTOS/main/ipt.sh | sudo bash 
 
 echo "Konfiguration abgeschlossen."
