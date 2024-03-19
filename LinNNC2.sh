258997d064bf2498b08b883394478157334b6fbb
lukasfreudensprung <127208026+lukasfreudensprung@users.noreply.github.com>
2024-03-19 19:57:05 +0100

    Create LinNNC2.sh

---
+++ LinNNC2.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+cat <<EOF | sudo tee /etc/netplan/00-installer-config.yaml > /dev/null
+network:
+  ethernets:
+    lan:
+      dhcp4: true
+      match:
+        macaddress: $1
+      set-name: lan
+    outside:
+      dhcp4: true
+      match:
+        macaddress: $2
+      set-name: outside
+  version: 2
+EOF
+
+sudo netplan apply
+
+echo "Konfiguration abgeschlossen."
