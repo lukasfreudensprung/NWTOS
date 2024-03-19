e729f84dc660c5a8bc58f79b640a0ddebd7338ae
lukasfreudensprung <127208026+lukasfreudensprung@users.noreply.github.com>
2024-03-19 19:46:18 +0100

    Update LinNNC1.sh

--- LinNNC1.sh
+++ LinNNC1.sh
@@ -1,1 +1,23 @@
+#!/bin/bash
 
+sudo hostnamectl set-hostname LinFreC1
+
+cat <<EOF | sudo tee /etc/netplan/00-installer-config.yaml > /dev/null
+network:
+  ethernets:
+    dmz:
+      dhcp4: true
+      match:
+        macaddress: $1
+      set-name: dmz
+    outside:
+      dhcp4: true
+      match:
+        macaddress: $2
+      set-name: outside
+  version: 2
+EOF
+
+sudo netplan apply
+
+echo "Konfiguration abgeschlossen."
